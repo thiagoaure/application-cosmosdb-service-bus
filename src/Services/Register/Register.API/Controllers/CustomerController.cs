5a2d38eb4f42091b94fbbedf7a964bbe08c63a02
Thiago Aureliano <thiagoh544@gmail.com>
2023-06-16 17:35:49 -0300

    add service bus publisher

--- src/Services/Register/Register.API/Controllers/CustomerController.cs
+++ src/Services/Register/Register.API/Controllers/CustomerController.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
 using Newtonsoft.Json;
-using Processor.API.Entities;
 using Register.API.DTOs;
+using Register.API.Helpers;
 using Register.API.Interfaces.Services;
+using System.Configuration;
+using System.Text;
+using System.Text.Json;
 
 namespace Processor.API.Controllers;
 [Route("api/customer")]
@@ -10,10 +15,12 @@ namespace Processor.API.Controllers;
 public class CustomerController
 {
     private readonly ICustomerService _customerService;
+    private readonly IServiceBus _serviceBus;
 
-    public CustomerController(ICustomerService customerService)
+    public CustomerController(ICustomerService customerService, IServiceBus serviceBus)
     {
         _customerService = customerService;
+        _serviceBus = serviceBus;
     }
 
     [HttpPost]
@@ -23,7 +30,9 @@ public class CustomerController
     {
         try
         {
-            var response = await _customerService.SaveCustomer( request );
+            var response = await _customerService.SaveCustomer(request);
+            await _serviceBus.SendMessageToQueue(request);
+
             return new OkObjectResult(response);
         }
         catch (Exception ex)
