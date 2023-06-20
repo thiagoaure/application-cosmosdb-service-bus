9175d9a1d7bdab4d12fcaf2aea4f56b703ae9af3
Thiago Aureliano <thiagoh544@gmail.com>
2023-06-19 22:41:35 -0300

    Revert "add README"
    This reverts commit b8ef5b980c8bb5ca298145c8812820084d759849.

--- src/Services/Register/GrpcService/Program.cs
+++ src/Services/Register/GrpcService/Program.cs
@@ -1,10 +1,4 @@
-using Grpc.Core;
-using Grpc.Net.Client;
-using GrpcService;
 using GrpcService.Services;
-using Microsoft.AspNetCore.Hosting.Server;
-using Microsoft.Azure.Cosmos;
-using Microsoft.Azure.ServiceBus;
 using Register.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +14,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.MapGrpcService<CustomerService>();
 
 
 app.Run();
